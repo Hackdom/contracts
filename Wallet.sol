90917f2e80e27ca0c4c18c63aec69b3e076390d2
Gav <gavin@parity.io>
2017-07-20 16:34:04 +0200

    Don't require the owner to be a signatory.

--- Wallet.sol
+++ Wallet.sol
@@ -60,13 +60,10 @@ contract multiowned {
 	/// @param _owners Array of authorized addresses
 	/// @param _required Number of sigs required
 	function multiowned(address[] _owners, uint _required) {
-		m_numOwners = _owners.length + 1;
-		m_owners[1] = uint(msg.sender);
-		m_ownerIndex[uint(msg.sender)] = 1;
-		for (uint i = 0; i < _owners.length; ++i)
-		{
-			m_owners[2 + i] = uint(_owners[i]);
-			m_ownerIndex[uint(_owners[i])] = 2 + i;
+		m_numOwners = _owners.length;
+		for (uint i = 0; i < _owners.length; ++i) {
+			m_owners[1 + i] = uint(_owners[i]);
+			m_ownerIndex[uint(_owners[i])] = 1 + i;
 		}
 		m_required = _required;
 	}
